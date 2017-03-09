75a09593cdc41e7d83534da782d27727f0525d5e
Sandney Farias da Cunha <sandneyfarias@hotmail.com>
2017-03-09 09:52:44 -0300

    Tasks

---
+++ ProjetoFinal/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  ProjetoFinal
+//
+//  Created by Student on 3/9/17.
+//  Copyright © 2017 Rodrigo Leandro. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
